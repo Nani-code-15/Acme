a0462626f0222efe0a5d8a223224b9c3968e4759
Naveen <naveenpanneer10@gmail.com>
2022-08-28 18:48:59 +0530

    create the contant page

--- src/assets/js/dashboard1.js
+++ src/assets/js/dashboard1.js
@@ -1,28 +1,132 @@
-$(function() {
-    "use strict";
+// $(function() {
+//     "use strict";
   
-     // chart 1
+//      // chart 1
    
-     var xValues = [340,200,300,400,500,600,700,800,900,1000];
+//      var xValues = ["jan", "feb","mar","apr","may","jun","jul","aug","sep","oct"];
 
-     new Chart("chart1", {
-       type: "line",
-       data: {
-         labels: xValues,
-         datasets: [{ 
-           data: [1090,1140,1060,1060,1070,1110,1330,2210,7830,6478],
-           borderColor: "red",
-           fill: false
-         }, { 
-           data: [2600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
-           borderColor: "green",
-           fill: false
-         }, { 
-           data: [300,700,2000,5000,6000,4000,2000,1000,200,100],
-           borderColor: "blue",
-           fill: false
-         }]
-       },
-    })
-});	 
-   
+//      new Chart("chart1", {
+//        type: "line",
+//        data: {
+//          labels: xValues,
+//          datasets: [{ 
+//            data: ["$5k","$10k","$15k","$20k","$25k","$30k","$35k","$40k","$45k","$50k"],
+//            borderColor: "red",
+//            fill: false
+//          }, { 
+//            data: ["$5k","$10k","$15k","$20k","$25k","$30k","$35k","$40k","$45k","$50k"],
+//            borderColor: "green",
+//            fill: false
+//          },]
+//        },
+//     })
+// });	 
+$(function() {
+  "use strict";
+
+   // chart 1
+ 
+    var ctx = document.getElementById('chart1').getContext('2d');
+  
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [{
+          label: 'New Visitor',
+          data: [15, 3, 8, 5, 7, 4, 6, 4, 6, 3],
+          backgroundColor: '#14abef',
+          borderColor: "transparent",
+          pointRadius :"3",
+          borderWidth: 2
+        }, {
+          label: 'Old Visitor',
+          data: [7, 5, 14, 7, 12, 6, 10, 6, 11, 5],
+          backgroundColor: "rgba(20, 171, 239, 0.35)",
+          borderColor: "transparent",
+          pointRadius :"3",
+          borderWidth: 1
+        }]
+      },
+    options: {
+      maintainAspectRatio: false,
+      legend: {
+        display: false,
+        labels: {
+        fontColor: '#585757',  
+        boxWidth:0
+        }
+      },
+      tooltips: {
+        displayColors:false
+      },	
+      scales: {
+        xAxes: [{
+        ticks: {
+          beginAtZero:true,
+          fontColor: '#585757'
+        },
+        gridLines: {
+          display: true ,
+          color: "rgba(0, 0, 0, 0.05)"
+        },
+        }],
+        yAxes: [{
+          display: true,
+          scaleLabel: {
+             show: true
+          },
+          gridLines:{
+             color:"#ecedef"
+          },
+          ticks: {
+             beginAtZero:true,
+             stepSize: 2,
+             fontColor:"#8f9092",
+             callback:function(value) {
+                 var x = ["$5k","$10k","$15k","$20k","$25k","$30k","$35k","$40k","$45k","$50k"]
+                   return x[value | 0];
+             }
+          }
+       }]
+       }
+
+     }
+    });  
+     
+    //chart 2
+
+    var ctx = document.getElementById("chart2").getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ["Direct", "Affiliate", "E-mail", "Other"],
+        datasets: [{
+          backgroundColor: [
+            "#14abef",
+            "#02ba5a",
+            "#d13adf",
+            "#fba540"
+          ],
+          data: [5856, 2602, 1802, 1105],
+          borderWidth: [0, 0, 0, 0]
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        cutoutPercentage: 60,
+         legend: {
+         position :"bottom",	
+         display: false,
+          labels: {
+            fontColor: '#ddd',  
+            boxWidth:1
+           }
+        }
+        ,
+        tooltips: {
+          displayColors:false
+        }
+         }
+    });
+});
